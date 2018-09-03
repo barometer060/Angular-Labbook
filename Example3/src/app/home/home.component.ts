a9d2dbd3041ed275bf63fef985e491aba6a5b361
frost060 <ajinkya060@gmail.com>
2018-09-03 08:26:44 +0530

    Example 3 First Commit

--- Example3/src/app/home/home.component.ts
+++ Example3/src/app/home/home.component.ts
@@ -1,35 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
-  addProduct: FormGroup
-  constructor(private fb: FormBuilder) { }
+  addProduct: FormGroup;
+  availableOptions: String[] = ["Big Bazar", "DMart", "Reliance", "Mega Store"];
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.addProduct = this.fb.group({
-      prodId: ['', Validators.required],
-      prodName: ['' , Validators.required],
-      prodCost: ['', Validators.required],
-      online: ['', Validators.required],
-      prodCat: [''],
-      available: ['']
+      prodId: ["", Validators.required],
+      prodName: ["", Validators.required],
+      prodCost: ["", Validators.required],
+      online: ["", Validators.required],
+      prodCat: [""],
+      available: [""]
     });
   }
 
   printData(e): void {
     e.preventDefault();
     if (this.addProduct.valid) {
-      console.log('Submitted');
+      console.log("Submitted");
     } else {
-      console.log('Required fields are missing');
+      console.log("Required fields are missing");
     }
-
   }
 
+  getChecked(e) {
+    if (e.target.checked) {
+      console.log(e.target.value);
+    }
+  }
+
+  getSelected(e) {
+    console.log(e.target.value);
+  }
 }
